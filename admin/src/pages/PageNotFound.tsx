4ef77657aee3f59406f25c7fcef2775f54754ee4
Trusthy136 <127469422+trungnpqph40302@users.noreply.github.com>
2025-06-18 21:13:05 +0700

    001 -  Base admin - layout

---
+++ admin/src/pages/PageNotFound.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
+const PageNotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
+      <h1 className="text-3xl font-bold mb-4">404 - Page Not Found</h1>
+      <p className="text-red-500 mb-6">
+        Không tìm thấy trang bạn yêu cầu. Vui lòng kiểm tra lại đường dẫn hoặc
+        quay lại trang chính.
+      </p>
+      <button
+        onClick={() => navigate("/")}
+        className="px-6 py-2 bg-[#0b08ab] text-white rounded-md hover:bg-[#0906a0] transition-colors"
+      >
+        Quay trở lại trang chủ
+      </button>
+    </div>
+  );
+};
+
+export default PageNotFound;
