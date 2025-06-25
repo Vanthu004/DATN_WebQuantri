5ec3112d2590df7ce84aeb85996ced61d98b7954
Vanthu204 <lythu2k4lc@gmail.com>
2025-06-25 21:08:14 +0700

    more product variant

--- src/models/product.js
+++ src/models/product.js
@@ -46,6 +46,10 @@ const productSchema = new mongoose.Schema({
     type: Number,
     default: 0
   },
+  views: {
+    type: Number,
+    default: 0
+  },
 });
 
 module.exports = mongoose.model("Product", productSchema);
