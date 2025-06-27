dc8a953bd1d25186947f25380c9b4af24bd43ef8
trungnpq <127469422+trungnpqph40302@users.noreply.github.com>
2025-06-27 22:03:27 +0700

    Danh sách đơn hàng, chuyển trạng thái đơn hàng

--- src/models/payment.js
+++ src/models/payment.js
@@ -1,42 +1,46 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const paymentSchema = new Schema({
-  payment_id: {
-    type: String,
-    required: true,
-    unique: true,
+const paymentSchema = new Schema(
+  {
+    payment_id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    order_id: {
+      type: String,
+      required: true,
+    },
+    payment_method: {
+      type: String,
+      enum: ["COD", "Banking"],
+      required: true,
+    },
+    transaction_code: {
+      type: String,
+      default: null,
+    },
+    payment_status: {
+      type: String,
+      enum: [
+        "Chưa thanh toán",
+        "Đã thanh toán",
+        "Chờ hoàn tiền",
+        "Đã hoàn tiền",
+      ],
+      default: "Chưa thanh toán",
+    },
+    amount_paid: {
+      type: Number,
+      required: true,
+    },
+    note: {
+      type: String,
+      default: "",
+    },
   },
-  order_id: {
-    type: String,
-    required: true,
-  },
-  payment_method: {
-    type: String,
-    enum: ["COD", "Banking", "CreditCard", "Paypal"], // có thể tùy biến
-    required: true,
-  },
-  transaction_code: {
-    type: String,
-    default: null,
-  },
-  payment_status: {
-    type: String,
-    enum: ["pending", "success", "failed"],
-    default: "pending",
-  },
-  amount_paid: {
-    type: Number,
-    required: true,
-  },
-  payment_date: {
-    type: Date,
-    default: Date.now,
-  },
-  note: {
-    type: String,
-    default: "",
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 module.exports = mongoose.model("Payment", paymentSchema);
