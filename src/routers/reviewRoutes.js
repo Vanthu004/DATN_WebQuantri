f7c00c21eb554ff9ba7a90e0ce2f882cff0be9bc
longvdph46585 <vlong285@gmail.com>
2025-06-30 14:52:37 +0700

    Thêm voucher, notification, cập nhật review

--- src/routers/reviewRoutes.js
+++ src/routers/reviewRoutes.js
@@ -2,19 +2,11 @@ const express = require("express");
 const router = express.Router();
 const reviewController = require("../controllers/reviewController");
 
-// Tạo review mới
-router.post("/reviews", reviewController.createReview);
-
-// Lấy tất cả review
-router.get("/reviews", reviewController.getReviews);
-
-// Lấy review theo ID
-router.get("/reviews/:id", reviewController.getReviewById);
-
-// Cập nhật review
-router.put("/reviews/:id", reviewController.updateReview);
-
-// Xóa review
-router.delete("/reviews/:id", reviewController.deleteReview);
+// Đúng: không lặp lại /reviews nữa
+router.post("/", reviewController.createReview);
+router.get("/", reviewController.getReviews);
+router.get("/:id", reviewController.getReviewById);
+router.put("/:id", reviewController.updateReview);
+router.delete("/:id", reviewController.deleteReview);
 
 module.exports = router;
