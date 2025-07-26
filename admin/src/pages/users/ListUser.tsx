3e6dfdd18769eb072e93d2b1dd4e1aaac71bc4f6
myxcs <118356147+myxcs@users.noreply.github.com>
2025-07-26 19:17:59 +0700

    small fix

--- admin/src/pages/users/ListUser.tsx
+++ admin/src/pages/users/ListUser.tsx
@@ -6,6 +6,7 @@ import api from "../../configs/api";
 import { blockUser } from "../../services/user";
 import { FaBan } from "react-icons/fa";
 
+
 const ListUser = () => {
   const [users, setUsers] = useState<User[]>([]);
   const [loading, setLoading] = useState(false);
@@ -123,7 +124,7 @@ const ListUser = () => {
                 <td>
                   {user.ban.bannedUntil
                     ? new Date(user.ban.bannedUntil).toLocaleString()
-                    : "Vĩnh viễn"}
+                    : "-" || "-"}
                 </td>
                 <td>{user.ban.reason || "-"}</td>
                 <td>
