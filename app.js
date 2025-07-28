d2b22960cd8a1ae0f16c9baf1eabaab2357284a9
DuAn_One <trindph53025@gmail.com>
2025-07-28 11:39:30 +0700

    tich hop thanh toan online

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use("/api/payments", paymentRoutes);
 app.use("/api/payment-methods", paymentRouter);
 app.use("/api/shipping-methods", shippingRouter);
 app.use("/api/product-variants", productVariantApi);
-app.use("/api/cart", cartApi);
+app.use("/api/carts", cartApi);
 app.use("/api/cart-items", cartItemApi);
 app.use("/api/statistics", statisticApi);
 app.use("/api/favorites", favoriteRouter);
