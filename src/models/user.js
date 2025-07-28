28de08cdd3881a99bbb0d57efc37669940b18f65
Vanthu204 <lythu2k4lc@gmail.com>
2025-07-28 23:28:54 +0700

    .

--- src/models/user.js
+++ src/models/user.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
     role: {
       type: String,
       enum: ["admin", "customer", "user"],
-      default: "customer",
+      default: "user",
     },
     phone_number: {
       type: String,
