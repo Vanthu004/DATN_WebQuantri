a84c482176633e20b005a714cea0ef0d8a3167c7
myxcs <118356147+myxcs@users.noreply.github.com>
2025-08-03 21:54:18 +0700

    fix backend

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      'react-native': 'react-native-web', // Ánh xạ react-native sang react-native-web
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-native'], // Giữ external như dự phòng
+    },
+  },
 });
