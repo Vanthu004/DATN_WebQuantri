e578c659f983e91f7c25c6cfcbf3c89461d4e31e
myxcs <118356147+myxcs@users.noreply.github.com>
2025-08-03 15:59:38 +0700

    cloudinary connected

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -9,7 +9,6 @@ const upload = require('../middlewares/uploadMiddleware');
 // Supabase routes
 router.get("/supabase-token", authMiddleware, userController.getSupabaseToken);
 
-
 // Public routes
 router.post("/register", userController.createUser);
 router.post("/login", userController.login);
@@ -28,6 +27,7 @@ router.put("/change-password", authMiddleware, userController.changePassword);
 router.get("/", authMiddleware, userController.getAllUsers);
 router.get("/all", userController.getAllUsers);
 router.get("/avatar/:id", userController.getAvatar);
+router.get("/messages", authMiddleware, userController.getMessages);
 router.get("/:id", authMiddleware, userController.getUserById);
 router.put("/:id", authMiddleware, userController.updateUser);
 router.delete("/:id", authMiddleware, userController.deleteUser);
@@ -39,7 +39,8 @@ router.patch("/:id/role", authMiddleware, userController.updateUserRole);
 module.exports = router;
 =======
 
-
+router.post('/upload-image', authMiddleware, upload.single('image'), userController.uploadImage);
+router.post('/send-message', authMiddleware, userController.sendMessage);
 
 module.exports = router;
 >>>>>>> 06654fd (supabase connected)
