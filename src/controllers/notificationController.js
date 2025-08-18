62d77384f4812778a38ae5afbb7dfe44fed83775
DuAn_One <trindph53025@gmail.com>
2025-08-18 13:10:47 +0700

    sủa tang thai

--- src/controllers/notificationController.js
+++ src/controllers/notificationController.js
@@ -13,7 +13,7 @@ exports.createNotification = async (req, res) => {
 exports.getAllNotifications = async (req, res) => {
   try {
     const notifications = await Notification.find()
-      .populate('user_id')
+      .populate('user_id', 'name email phone_number')
       .populate('order_id');
     res.status(200).json(notifications);
   } catch (err) {
@@ -24,7 +24,7 @@ exports.getAllNotifications = async (req, res) => {
 exports.getNotificationById = async (req, res) => {
   try {
     const notification = await Notification.findById(req.params.id)
-      .populate('user_id')
+      .populate('user_id', 'name email phone_number')
       .populate('order_id');
     if (!notification) return res.status(404).json({ message: 'Notification not found' });
     res.status(200).json(notification);
