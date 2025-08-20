644efd6593c850f78f959e04b0d66b4025fd8026
Vanthu204 <lythu2k4lc@gmail.com>
2025-08-20 13:48:14 +0700

    resolve thai

--- src/models/user.js
+++ src/models/user.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["user", "staff", "admin", "customer"], 
+      enum: ["user", "staff", "admin"], 
       default: "user",
     },
     phone_number: {
